167bcf17e0cfe0712a3e5fdc45b255c26e26a2b8
unknown <ameliashort331@yahoo.com>
2020-07-14 18:13:44 -0400

    Created tables to store meeting and user information independently

--- queries.js
+++ queries.js
@@ -32,7 +32,7 @@ const getAllMeetingLogs = (request, response) => {
 }
 
 const getActiveMeetingLogs = (request, response) => {
-  pool.query("SELECT * FROM meeting_logs WHERE meeting_ended=false AND meeting_host=true AND user_type='researcher' ORDER BY meeting_id ASC", (error, results) => {
+  pool.query("SELECT * FROM meetings WHERE meeting_ended = false;", (error, results) => {
     if (error) {
       throw error
     }
@@ -44,7 +44,11 @@ const getMeetingLog = (request, response) => {
   const user_name = request.params.user_name
   const meeting_number = parseInt(request.params.meeting_number)
 
-  pool.query('SELECT * FROM meeting_logs WHERE user_name = $1 AND meeting_number = $2;', [user_name, meeting_number], (error, results) => {
+  pool.query(`SELECT log_id, meeting_logs.meeting_id, meeting_logs.user_id, meeting_join_time, meeting_leave_time, meeting_host, user_name, meeting_number
+      FROM meeting_logs
+      LEFT JOIN users ON meeting_logs.user_id = users.user_id
+      LEFT JOIN meetings ON meeting_logs.meeting_id = meetings.meeting_id
+      WHERE user_name = $1 AND meeting_number = $2;`, [user_name, meeting_number], (error, results) => {
     if (error) {
       throw error
     }
@@ -56,11 +60,30 @@ const getOtherUsersForMeeting = (request, response) => {
   const user_name = request.params.user_name
   const meeting_number = parseInt(request.params.meeting_number)
 
-  pool.query('SELECT UNIQUE(user_name) FROM meeting_logs WHERE meeting_number = $1 AND meeting_ended=false AND user_name!= $2 AND meeting_leave_time = NULL;', [meeting_number, user_name], (error, results) => {
+  pool.query(`SELECT DISTINCT user_name
+      FROM meeting_logs
+      LEFT JOIN meetings on meeting_logs.meeting_id = meetings.meeting_id
+      LEFT JOIN users on meeting_logs.user_id = users.user_id
+      WHERE meeting_number = $1 AND meeting_ended=false AND user_name!= $2 AND meeting_leave_time = NULL;`,
+      [meeting_number, user_name], (error, results) => {
     if (error) {
       throw error
     }
     response.status(200).json(results.rows)
+  })
+}
+
+const createMeeting = (request, response) => {
+  const meeting_number = request.body.meeting_number;
+  const meeting_password = request.body.meeting_password;
+
+  pool.query('INSERT INTO meetings (meeting_number, meeting_password, meeting_ended) values ($1, $2, $3) ON CONFLICT (meeting_number) DO NOTHING',
+   [meeting_number, meeting_password, false], (error, results) => {
+    if (error) {
+      console.log(request);
+      throw error
+    }
+    response.status(201).send(`Meeting created with ID: ${results.insertId}`)
   })
 }
 
@@ -73,26 +96,65 @@ const createMeetingLog = (request, response) => {
   const user_type = request.body.user_type;
   const meeting_host = request.body.meeting_host;
 
-  pool.query('INSERT INTO meeting_logs (meeting_number, meeting_password, user_name, email, ip_address, meeting_join_time, meeting_leave_time, user_type, meeting_host, meeting_ended) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);', [meeting_number, meeting_password, user_name, email, ip_address, new Date(), null, user_type, meeting_host, false], (error, results) => {
+  //add user to the users table if necessary
+  pool.query(`INSERT INTO users (user_name, user_email, user_ip_address, user_type)
+      SELECT $1, $2, $3, $4 WHERE NOT EXISTS
+      (SELECT * FROM users WHERE user_name = $1::VARCHAR AND user_email= $2::VARCHAR LIMIT 1);`,
+      [user_name, email, ip_address, user_type], (error, results) => {
+    if (error) {
+      console.log(request);
+      throw error
+    }
+  })
+
+  //add log data to meeting_logs table
+  pool.query(`INSERT INTO meeting_logs (meeting_id, meeting_join_time, meeting_leave_time, meeting_host, user_id)
+    SELECT
+      (SELECT meeting_id from meetings WHERE meeting_number = $1),
+      $2, $3, $4,
+      (SELECT user_id FROM users WHERE user_name = $5 AND user_email = $6);`,
+      [meeting_number, new Date(), null, meeting_host, user_name, email], (error, results) => {
     if (error) {
       console.log(request);
       throw error
     }
     response.status(201).send(`Meeting Log added with ID: ${results.insertId}`)
   })
+
 }
 
-const updateMeetingLogEndedServer = (user_name, meeting_num) => {
+const updateMeetingLog = (user_name, meeting_num) => {
   const meeting_number = parseInt(meeting_num)
 
   pool.query(
-    'WITH user_logs as (SELECT * FROM meeting_logs WHERE user_name = $1 AND meeting_number = $2 ORDER BY meeting_join_time DESC LIMIT 1) UPDATE meeting_logs SET meeting_ended = true, meeting_leave_time = $3 WHERE meeting_id in (SELECT meeting_id from user_logs);',
+    `WITH user_logs as
+    (SELECT log_id, meeting_logs.meeting_id, meeting_logs.user_id, meeting_join_time, meeting_leave_time, meeting_host, user_name, meeting_number
+        FROM meeting_logs
+        LEFT JOIN users ON meeting_logs.user_id = users.user_id
+        LEFT JOIN meetings ON meeting_logs.meeting_id = meetings.meeting_id
+        WHERE user_name = $1 AND meeting_number = $2 ORDER BY meeting_join_time DESC LIMIT 1)
+    UPDATE meeting_logs set meeting_leave_time = $3 WHERE user_id = (SELECT user_id from users where user_name = $1)
+     AND meeting_leave_time IS NULL;`,
     [user_name, meeting_number, new Date()],
     (error, results) => {
       if (error) {
         throw error
       }
-      return(`Set meeting_ended=true for Meeting Number: ${meeting_number} and User:${user_name}`)
+      return(`User:${user_name} has left meeting: ${meeting_number}.`)
+    }
+  )
+}
+
+const endMeeting = (meeting_num) => {
+  const meeting_number = parseInt(meeting_num)
+  
+  pool.query('UPDATE meetings SET meeting_ended = true WHERE meeting_number = $1;',
+    [meeting_number],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      return(`Meeting Number: ${meeting_number} has ended.`)
     }
   )
 }
@@ -112,8 +174,10 @@ module.exports = {
   getAllMeetingLogs,
   getActiveMeetingLogs,
   getMeetingLog,
+  createMeeting,
   createMeetingLog,
   deleteMeetingLog,
-  updateMeetingLogEndedServer,
+  updateMeetingLog,
   createAndMigrateDB,
+  endMeeting
 }
