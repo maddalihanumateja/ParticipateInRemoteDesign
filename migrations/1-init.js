7951b825f47a4f766c4f097695d8ba71295cf26e
Amelia Short <ameliashort331@yahoo.com>
2020-09-24 14:01:37 -0400

    Updated 1-init.js file

--- migrations/1-init.js
+++ migrations/1-init.js
@@ -1,7 +1,10 @@
 const createRoleEnum = require("./helpers/create-role-enum");
-const createMeetingLogsTable = require("./helpers/create-meeting-logs-table");
-const insertTestData = require("./helpers/insert-test-data");
+const createRoleEnum = require("./helpers/create-meetings-table");
+const createRoleEnum = require("./helpers/create-logs-table");
+const createRoleEnum = require("./helpers/create-users-table");
+// const createMeetingLogsTable = require("./helpers/create-meeting-logs-table");
+// const insertTestData = require("./helpers/insert-test-data");
 
-module.exports.generateSql = () => `${createRoleEnum} 
-${createMeetingLogsTable} 
+module.exports.generateSql = () => `${createRoleEnum}
+${createMeetingLogsTable}
 ${insertTestData}`
