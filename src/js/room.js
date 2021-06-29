a37aaf566bd83aae0138c39fec253abfdbd7ec46
Hanuma Teja Maddali <maddalihanumateja@gatech.edu>
2021-06-29 15:18:09 -0400

    Working devices button
    Only updates when users are added. Needs to update when users are removed

--- src/js/room.js
+++ src/js/room.js
@@ -36,6 +36,9 @@ socket.on('existing_users', function(obj){
         //modal_append(users_in_room[users_in_room.length - 1]);
       }
       for(var socketId in obj){
+      	if(USER_TYPE == "researcher"){
+      		modal_append(obj[socketId]);
+      	}
       	call(obj[socketId], socketId);
       }
 });
@@ -44,8 +47,8 @@ socket.on('room_join_event', function(obj){
       console.log(obj['users_in_room']);
       users_in_room = obj['users_in_room'].slice();
       console.log(obj['message']);
-      if (users_in_room.length > 1) {
-        //modal_append(users_in_room[users_in_room.length - 1]);
+      if (users_in_room.length > 1 && USER_TYPE == "researcher") {
+        modal_append(obj['new_username']);
       }
 
       call(obj['new_username'], obj['new_peer_id']);
@@ -139,7 +142,7 @@ let modalsText2;
 modal.innerHTML = '';
 
 if (devices.includes("printer")) {
-  let modalsText2 = $('<div><img src=\'https://icons-for-free.com/iconfiles/png/512/interface+multimedia+print+printer+icon-1320185667007730348.png\' style = \'max-height:70px;float:left\' /><span style =\'font-size:50px;padding-left:2%;\'>Printer</span><br><br><label for=\'participants\'>Choose a participant:</label><br><select name=\'participants\' class=\'participant-list\'></select><br><br><p>Upload a file:<form method=\'post\' action=\'upload\' enctype=\'multipart/form-data\'><input type=\'file\' name=\'avatar\'><input type=\'submit\'></form></p></div>');
+  let modalsText2 = $('<div><i class="fa fa-print" style = \'max-height:70px;float:left\'></i><span style =\'font-size:50px;padding-left:2%;\'>Printer</span><br><br><label for=\'participants\'>Choose a participant:</label><br><select name=\'participants\' class=\'participant-list\'></select><br><br><p>Upload a file:<form method=\'post\' action=\'upload\' enctype=\'multipart/form-data\'><input type=\'file\' name=\'avatar\'><input type=\'submit\'></form></p></div>');
   $(modalsText2).appendTo(modal);
   let nameElement = $('<option value=\'' + name + '\'>' + name + '</option>');
   var content = document.getElementsByClassName("participant-list");
@@ -181,57 +184,54 @@ var observerConfig = {
     childList: true
 };
 
- /*
+const iElement = $('<input id=\"file-input\" type=\"file\" style=\"display: none;\" />');
+$(iElement).appendTo('#mtg-root');
 
-        const iElement = $('<input id=\"file-input\" type=\"file\" style=\"display: none;\" />');
-        $(iElement).appendTo('#mtg-root');
+//const container = document.querySelector('div.meeting-client-inner');
+//observer.observe(container.childNodes[0], observerConfig);
 
-        const container = document.querySelector('div.meeting-client-inner');
-        observer.observe(container.childNodes[0], observerConfig);
-
-        console.log('join meeting success!');
+console.log('join meeting success!');
 
 
 
-        if (USER_TYPE == 'researcher') {
-          let modals = $('<div id=\'myModal\' class=\'modal\' style = \'display: none;position: fixed;z-index: 1;padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);\'></div>');
-          let modalInner = $('<div class=\'modal-content\' id=\'modal-content\' style = \'background-color: #fefefe;float:right;padding: 20px;border: 1px solid #888;width: 500px;\'><span class=\'close\'>&times;</span></div>');
-          let modalsText = $('<span>No devices found.</span>');
+if (USER_TYPE == 'researcher') {
+  let modals = $('<div id=\'myModal\' class=\'modal\' style = \'display: none;position: fixed;z-index: 1;padding-top: 100px;left: 0;top: 0;width: 100%;height: 100%;overflow: auto;background-color: rgb(0,0,0);background-color: rgba(0,0,0,0.4);\'></div>');
+  let modalInner = $('<div class=\'modal-content\' id=\'modal-content\' style = \'background-color: #fefefe;float:right;padding: 20px;border: 1px solid #888;width: 500px;\'><span class=\'close\'>&times;</span></div>');
+  let modalsText = $('<span>No devices found.</span>');
 
-          let footerLeft = document.getElementById("wc-footer-left");
+  let footerRight = document.getElementById("footer-right");
 
-          let bElement = $('<div class = \'send-video-container left-tool-item\'><img src = \'https://imgur.com/a/JzmeMyR\' id=\'project\' style=\'max-width: 90%;max-height: 70%;\' /><br>Devices<span class=\'loading\' style=\'display: none;\'></span></div>');
-          $(modalsText).appendTo(modalInner);
-          $(modalInner).appendTo(modals);
-          $(bElement).appendTo(footerLeft);
-          $(modals).appendTo("body");
+  let bElement = $('<div id="project" class = \'options__button\'><i class="fa fa-connectdevelop"></i><span class=\'loading\' style=\'display: none;\'></span></div>');
+  $(modalsText).appendTo(modalInner);
+  $(modalInner).appendTo(modals);
+  $(bElement).appendTo(footerRight);
+  $(modals).appendTo("body");
 
-          var modal = document.getElementById("myModal");
+  var modal = document.getElementById("myModal");
 
-          // Get the button that opens the modal
-          var btn = document.getElementById("project");
+  // Get the button that opens the modal
+  var btn = document.getElementById("project");
 
-          // Get the <span> element that closes the modal
-          var span = document.getElementsByClassName("close")[0];
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
 
-          // When the user clicks the button, open the modal
-          btn.onclick = function() {
-            modal.style.display = "block";
-          }
+  // When the user clicks the button, open the modal
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
 
-          // When the user clicks on <span> (x), close the modal
-          span.onclick = function() {
-            modal.style.display = "none";
-          }
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
 
-          // When the user clicks anywhere outside of the modal, close it
-          window.onclick = function(event) {
-            if (event.target == modal) {
-              modal.style.display = "none";
-            }
-          }
-        }
-        */
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
 
 
 let myVideoStream;
